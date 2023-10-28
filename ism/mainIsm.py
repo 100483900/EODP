d6699fd8673dfd75814b24cf4b9c2cdf5b990c63
Adrian Calleja <100483900@alumnos.uc3m.es>
2023-10-28 21:23:01 +0200

    Add E2E test

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -6,8 +6,10 @@ import os
 
 auxdir = 'auxiliary'
 indir = "EODP_TER/EODP-TS-ISM/input/gradient_alt100_act150"
+indir_e2e = "EODP_TER/EODP-TS-E2E/sgm_out"
+outdir_e2e = "EODP_TER/EODP-TS-E2E/output"
 outdir = "EODP_TER/EODP-TS-ISM/output"
 
 # Initialise the ISM
-myIsm = ism(auxdir, indir, outdir)
+myIsm = ism(auxdir, indir_e2e, outdir)
 myIsm.processModule()
